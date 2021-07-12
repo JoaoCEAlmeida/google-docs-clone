cd0ae2b0b48ec6c57d43a87be857ede509535e7e
johnnygato <joao88vsc@gmail.com>
2021-07-12 09:28:24 +0100

    build documents layout section, add create documents feature and store it on firestore

--- components/DocumentsSection.js
+++ components/DocumentsSection.js
@@ -1,15 +1,38 @@
 import Icon from "@material-tailwind/react/Icon";
 
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import db from "../firebase";
+import { useSession } from "next-auth/client";
+
+import DocumentRow from "./DocumentRow";
+
 const DocumentsSection = () => {
+  const [session] = useSession();
+
+  const [snapshot] = useCollectionOnce(
+    db
+      .collection("userDocs")
+      .doc(session?.user?.email)
+      .collection("docs")
+      .orderBy("timestamp", "desc")
+  );
+
   return (
-    <section className="bg-white px-10">
-      <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
-        <div className="flex items-center justify-between pb-5">
-          <h2 className="font-medium flex-grow">My Documents</h2>
-          <p className="mr-12">Date Created</p>
-          <Icon name="folder" size="3xl" color="gray" />
-        </div>
+    <section className="bg-white px-10 max-w-3xl mx-auto py-8 text-sm text-gray-700">
+      <div className="flex items-center justify-between pb-5">
+        <h2 className="font-medium flex-grow">My Documents</h2>
+        <p className="mr-12">Date Created</p>
+        <Icon name="folder" size="3xl" color="gray" />
       </div>
+
+      {snapshot?.docs.map((doc) => (
+        <DocumentRow
+          key={doc.id}
+          id={doc.id}
+          fileName={doc.data().fileName}
+          date={doc.data().timestamp}
+        />
+      ))}
     </section>
   );
 };
--- components/NewDocumentSection.js
+++ components/NewDocumentSection.js
@@ -1,11 +1,65 @@
+import { useState } from "react";
+
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+
 import Image from "next/image";
 
+import db from "../firebase";
+import { useSession } from "next-auth/client";
+import firebase from "firebase";
+
 const NewDocumentSection = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
+  const [session] = useSession();
+
+  const createDocument = () => {
+    if (!input) return;
+
+    db.collection("userDocs").doc(session?.user?.email).collection("docs").add({
+      fileName: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+    setShowModal(false);
+  };
+
+  const modal = (
+    <Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="outline-none w-full"
+          placeholder="Enter name of document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={() => setShowModal(false)}
+        >
+          Cancel
+        </Button>
+        <Button color="blue" ripple="light" onClick={createDocument}>
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+
   return (
-    <section className="bg-[#F8F9FA] pb-10 px-10">
-      <div className="max-w-3xl mx-auto">
+    <>
+      {modal}
+      <section className="bg-[#F8F9FA] pb-10 px-10 max-w-3xl mx-auto">
         <div className="flex items-center justify-between py-5">
           <h2 className="text-gray-700 text-lg">Start a new document</h2>
           <Button
@@ -20,14 +74,15 @@ const NewDocumentSection = () => {
           </Button>
         </div>
 
-        <div>
-          <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
-            <Image src="https://links.papareact.com/pju" layout="fill" />
-          </div>
-          <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+        <div
+          className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700"
+          onClick={() => setShowModal(true)}
+        >
+          <Image src="https://links.papareact.com/pju" layout="fill" />
         </div>
-      </div>
-    </section>
+        <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+      </section>
+    </>
   );
 };
 
