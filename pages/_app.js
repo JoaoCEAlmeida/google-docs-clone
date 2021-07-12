13caeba9fe78ce0d25c7b558187361a974764813
johnnygato <joao88vsc@gmail.com>
2021-07-12 17:47:10 +0100

    add loding bar on top

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,20 @@ import Head from "next/head";
 import "@material-tailwind/react/tailwind.css";
 import "tailwindcss/tailwind.css";
 import "../styles.css";
+import NextNprogress from "nextjs-progressbar";
 
 import { Provider, session } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        options={{ showSpinner: false }}
+      />
       <Head>
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
