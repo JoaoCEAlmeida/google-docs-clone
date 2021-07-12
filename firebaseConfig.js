302be8e036b105542b6c1984a0d5ddece609297b
johnnygato <joao88vsc@gmail.com>
2021-07-12 13:26:26 +0100

    refactor

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "docs-clone-af0cf.firebaseapp.com",
+  projectId: "docs-clone-af0cf",
+  storageBucket: "docs-clone-af0cf.appspot.com",
+  messagingSenderId: "479029348051",
+  appId: "1:479029348051:web:e18e8bb908a7f7d144f626",
+};
+
+export default firebaseConfig;
